680c9d4cb89e9f0eb9f6e9b96966a7f6df850403
Ville Backelind <villebergbackelind@gmail.com>
2024-08-27 19:52:50 +0200

    Man function test build
    built a concept of the main func

--- main.go
+++ main.go
@@ -1,6 +1,24 @@
 package main
 
-import {
+import (
 	"fmt"
-	""
+	"net"
+)
+
+func main() {
+	p := 80
+	serv := "scanme.nmap.org"
+	tcpPortChecker(serv, p)
+
+}
+
+func tcpPortChecker(serv string, p int) {
+	servNport := fmt.Sprintf(serv+":%v", p)
+	connex, err := net.Dial("tcp", servNport)
+	if err != nil {
+		return
+	}
+	defer connex.Close()
+	o := fmt.Sprintf("%v, is Open", p)
+	fmt.Println(o)
 }
