a34affe7512e2c9c19a464c4e85deae3db974da1
Ville Backelind <villebergbackelind@gmail.com>
2024-08-28 22:46:21 +0200

    implement goroutine
    implemented goroutine

--- main.go
+++ main.go
@@ -11,13 +11,23 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 func main() {
-	p := 80
+	portList := []int{20,21,22,23,24,25,26,27,28,29,30}
 	serv := "scanme.nmap.org"
-	tcpPortChecker(serv, p)
-
+	var wg sync.WaitGroup
+	
+	for _, P := range portList {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			tcpPortChecker(serv, P)
+		}()
+		
+	}
+	wg.Wait()
 }
 
 func tcpPortChecker(serv string, p int) {
@@ -27,5 +37,11 @@ func tcpPortChecker(serv string, p int) {
 		return
 	}
 	defer connex.Close()
-	fmt.Printf(`%v, is Open`, p)
+	fmt.Printf("%v, is Open\n", p)
 }
+
+
+
+
+
+
