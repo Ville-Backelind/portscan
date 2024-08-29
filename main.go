3fa8df45e31b752e2134f0b90c1a1b14c3d2649e
Ville Backelind <villebergbackelind@gmail.com>
2024-08-29 20:45:14 +0200

    added flags
    added flags that are parsed for starting and ending flags and URL

--- main.go
+++ main.go
@@ -9,23 +9,28 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"sync"
 )
 
 func main() {
-	portList := []int{20,21,22,23,24,25,26,27,28,29,30}
-	serv := "scanme.nmap.org"
+
+	urlFlag := flag.String("url", "scanme.nmap.org", "Give an URL or IP to be scanned")
+	portStartFlag := flag.Int("start", 1, "Starting port number")
+	portEndFlag := flag.Int("end", 65535, "Ending port number")
+	flag.Parse()
+
 	var wg sync.WaitGroup
-	
-	for _, P := range portList {
+
+	for p := *portStartFlag; p <= *portEndFlag; p++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			tcpPortChecker(serv, P)
+			tcpPortChecker(*urlFlag, p)
 		}()
-		
+
 	}
 	wg.Wait()
 }
@@ -33,15 +38,10 @@ func main() {
 func tcpPortChecker(serv string, p int) {
 	servNport := fmt.Sprintf(serv+":%v", p)
 	connex, err := net.Dial("tcp", servNport)
+
 	if err != nil {
 		return
 	}
 	defer connex.Close()
 	fmt.Printf("%v, is Open\n", p)
 }
-
-
-
-
-
-
